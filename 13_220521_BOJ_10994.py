3c418657dbf98bd9b772220c55a8a70b708228d1
NQ-OO <lol1219@snu.ac.kr>
2022-05-25 13:44:23 +0900

    220521 submit

--- 13_220521_BOJ_10994.py
+++ 13_220521_BOJ_10994.py
@@ -1,21 +1,25 @@
 
+
 input_num = int(input())
 
-frame = [[[] for _ in range(1 + (input_num-1)*4)] for _ in range(1 + (input_num-1)*4)]
+frame_len = input_num*4 - 3
+frame = [[" " for __ in range(frame_len)] for _ in range(frame_len)]
 
-# for idx in range(1 + (input_num-1)*4) : 
-#   print(frame[idx])
-  
-def recur(input_num, idx) : 
-  if (input_num-idx) == 0 : 
-    return
+def recur(n) : 
+  if n == (frame_len//2 + 1) :
+    frame[frame_len//2][frame_len//2] ="*"
   else : 
-    start_point = frame[input_num//2+1 - (2 * idx)][input_num//2+1 - (2 * idx)]
-    for star_idx in range(1 + (idx-1)*4) : 
-      frame[input_num//2+1 - (2 * idx)][input_num//2+1 - (2 * idx)].append("*")
-      
+    start_point = 2*n
+    side_len = (frame_len - n*4)
+    for idx_num in range(start_point, start_point+ side_len) :
+      frame[start_point][idx_num] = "*" ## top
+      frame[start_point+side_len-1][idx_num] = "*" ##bottom
+      frame[idx_num][start_point] = "*" ##left
+      frame[idx_num][start_point + side_len-1] = "*" ##right
+    recur(n+1)
 
 
-recur(input_num, 0)
+recur(0)
+
 for idx in range(1 + (input_num-1)*4) : 
-  print(frame[idx])
+  print("".join(frame[idx]))
