722024fb44c4a819ad0d71aab67f77f24d146f48
NQ-OO <lol1219@snu.ac.kr>
2022-07-19 11:27:05 +0900

    220719 submit

---
+++ 70_220719_porgrammers_멀쩡한사각형.py
@@ -0,0 +1,15 @@
+# link : https://school.programmers.co.kr/learn/courses/30/lessons/62048
+def solution(w,h):
+    answer = 1
+    def GCD(a, b) :   
+        if a % b == 0:
+            return b
+        elif b == 0:
+            return a
+        else:
+            return GCD(b, a % b)
+
+    new_gcd = GCD(w, h)
+    cross_block_num = (w/new_gcd + h/new_gcd - 1) * new_gcd
+    answer = w * h - cross_block_num
+    return answer
